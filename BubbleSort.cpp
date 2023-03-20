d5c7ed1d3f22ca5c02834fc26f8558f3fb320716
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2023-03-20 20:56:48 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,8 +1,14 @@
-//TC:O(N^2)
+//Worst Case TC:O(N^2)
+//Best case TC: O(N)
 //SC:O(1)
+bool swapped = false;
 for(int i=1; i<n; i++){
     for(int j=0; j<n-i; j++){
-      if(arr[j]>arr[j+1])
+      if(arr[j]>arr[j+1]){
+          swapped = true;
           swap(arr[j],arr[j+1]);
     }
   }
+   if(swapped == false)
+        break;
+}
